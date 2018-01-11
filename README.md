54630f1b3417337bfb28c7db2f9add335e462589
John2013 <NaWasington@yandex.ru>
2018-01-11 19:03:51 +0300

    commit

--- README.md
+++ README.md
@@ -1,6 +1,30 @@
 # Frequency Analysis of Words
 
-[TODO. There will be project description]
+Displays a list of the most repeated words in a text file
+
+# How to run
+
+```bash
+$ python lang_frequency  # or python3
+```
+
+Example:
+```bash
+$ python lang_frequency
+Enter the file path: book1.txt
+The most popular words in this file:
+[('и', 10200),
+ ('–', 8186),
+ ('в', 5285),
+ ('не', 4383),
+ ('что', 3698),
+ ('на', 3373),
+ ('он', 3178),
+ ('с', 3087),
+ ('как', 2075),
+ ('я', 1836)]
+```
+
 
 # Project Goals
 
--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,27 @@
+from collections import Counter
+from pprint import pprint
+
+from os.path import isfile
 
 
 def load_data(filepath):
-    pass
+    with open(filepath, encoding="utf-8") as file:
+        return "".join(file.readlines())
 
 
 def get_most_frequent_words(text):
-    pass
+    return Counter(text.lower().split()).most_common(10)
 
 
 if __name__ == '__main__':
-    pass
+    while True:
+        filepath = input("Enter the file path: ")
+        if isfile(filepath):
+            break
+        else:
+            print("There is no file at the specified path")
+
+    text = load_data(filepath)
+
+    print("The most popular words in this file:")
+    pprint(get_most_frequent_words(text))
